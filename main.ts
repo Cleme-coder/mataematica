52e06b61c8b6d648f2a7aac1414c215612da56d6
Cleme-coder <67659694+Cleme-coder@users.noreply.github.com>
2020-08-23 17:01:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(" mis figuras gemometricas son 3 cubos y 2 cilindros")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showString("hola soy wall-c")
+basic.forever(function () {
+	
+})
